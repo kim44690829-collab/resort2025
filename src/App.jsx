f245725fb54d545ad9b6ac04d0c3c8f487a06181
jhoj0423 <singiru335@gmail.com>
2025-12-07 22:40:08 +0900

    2025-12-07 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 import ResortDate from './Api/ResortDate'
+import Room from './Page/Room'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <ResortDate />
+      <Room />
     </>
   )
 }
